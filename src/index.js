568f851e01b38ec233da081c779c3944cb152e28
Michal Kudrnac <michalkud@gmail.com>
2018-12-09 17:38:07 +0100

    Auth middleware wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
 const { express: voyagerMiddleware } = require('graphql-voyager/middleware');
+const bodyParser = require('body-parser');
+const gql = require('graphql-tag');
 
 const resolvers = {
   Query: {
@@ -40,6 +42,43 @@ const server = new GraphQLServer({
     }),
   }),
 });
+/*
+const isOrdersQueryAsked = query => gql`${query}`
+  .definitions
+    .some(definition =>
+      definition.selectionSet.selections
+        .some(selection => console.log(selection.name)));
 
+server.express.use(bodyParser.json());
+server.express.post(
+  server.options.endpoint,
+  (req, res, next) => {
+    if (isOrdersQueryAsked(req.body.query)) {
+      return checJwt(req, res, next);
+    }
+    return next();
+  },
+  (err, req, res, next) => {
+    if (err) return res.status(401).send(err.message);
+    next();
+  }
+);*/
+
+const authMiddleware = (req, res, next) => {
+  console.log('inside middleware')
+
+  const askedFields = gql`${req.body.query}`
+  .definitions
+    .map(definition =>
+      definition.selectionSet.selections
+        .map(selection => selection));
+
+  console.log(askedFields);
+
+  next();
+}
+
+server.express.use(bodyParser.json());
+server.express.use(authMiddleware);
 server.express.use('/voyager', voyagerMiddleware({ endpointUrl: 'http://localhost:4466' }));
 server.start(() => console.log(`GraphQL server is running on http://localhost:4000`));
