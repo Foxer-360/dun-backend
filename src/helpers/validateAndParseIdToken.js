8fbbee283424138ef50827f8ff896f2661872ff8
Michal Kudrnac <michalkud@gmail.com>
2019-01-11 12:58:33 +0100

    Checking if we have key and public key in parse token function.

--- src/helpers/validateAndParseIdToken.js
+++ src/helpers/validateAndParseIdToken.js
@@ -15,6 +15,7 @@ const validateAndParseIdToken = idToken => new Promise((resolve, reject) => {
   if (!header || !header.kid || !payload) return reject(new Error('Invalid Token'));
   return jwks.getSigningKey(header.kid, (getSigningKeyError, key) => {
     if (getSigningKeyError) reject(new Error(`Error getting signing key: ${getSigningKeyError.message}`));
+    if (!key || !key.publicKey) reject(new Error('Error getting signing key: no key or public key'));
     jwt.verify(idToken, key.publicKey, { algorithms: ['RS256'] }, (jwtVerifyError, decoded) => {
       if (jwtVerifyError) return reject(new Error(`jwt verify error: ${jwtVerifyError.message}`));
       return resolve(decoded);
